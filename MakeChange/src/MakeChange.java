f7c8890cf4ac4664392d96176ad15b80808fbffa
904238003 <904238003@L829WD150099.edu.cbe.ab.ca>
2021-03-17 13:42:36 -0600

    Got alot done

--- MakeChange/src/MakeChange.java
+++ MakeChange/src/MakeChange.java
@@ -1,4 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class MakeChange {
 
+	public static void main(String[] args) throws Exception{
+		
+		double TOONIEVALUE = 2;
+		int LOONIEVALUE = 1;
+		double QUARTERVALUE = 0.25;
+		double DIMEVALUE = 0.10;
+		double NICKELVALUE = 0.05;
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Enter a dollar amount");
+		 
+		String input = reader.readLine();
+		double number = Double.parseDouble(input);
+		
+		int numberOfToonies = (int)number / (int)TOONIEVALUE;
+		double remander = (number % TOONIEVALUE);
+		
+		int numberOfLoonies = (int)remander / (int)LOONIEVALUE;
+		remander = (remander % LOONIEVALUE);
+		
+		double numberOfQuarters = remander / QUARTERVALUE;
+		remander = (remander % QUARTERVALUE);
+		
+		double numberOfDimes = remander / DIMEVALUE;
+		remander = (remander % DIMEVALUE);
+		
+		double numberOfNickels = remander / NICKELVALUE;
+		remander = (remander % NICKELVALUE);
+		
+		System.out.printf("toonies:" + numberOfToonies + "; loonies:" + numberOfLoonies + "; quarters:" + numberOfQuarters + "; dimes:1; nickels:1");
+	}
 }
+
