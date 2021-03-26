b24ce2bf585d98e4daca2f58729c64620149eda8
isaiahgrills <isaiahgrills@10.0.0.166>
2021-03-26 11:52:44 -0600

    Just cleaning it up to work better

--- MakeChange/src/MakeChange.java
+++ MakeChange/src/MakeChange.java
@@ -10,35 +10,45 @@ public class MakeChange {
 		final double QUARTERVALUE = 0.25;
 		final double DIMEVALUE = 0.10;
 		final double NICKELVALUE = 0.05;
+		double number = 0;
+		boolean works = false;
 
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("What is your dollar amount?:");
 		try {
-			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-			System.out.print("What is your dollar amount?:");
 
 			String input = reader.readLine();
-			double number = Double.parseDouble(input);
-			number = NICKELVALUE*(Math.round(number/NICKELVALUE));
+			number = Double.parseDouble(input);
+			works = true;
 
-			int numberOfToonies = (int)number / (int)TOONIEVALUE;
-			double remander = (number % TOONIEVALUE);
-
-			int numberOfLoonies = (int)remander / (int)LOONIEVALUE;
-			remander = (remander % LOONIEVALUE);
-
-			double numberOfQuarters = remander / QUARTERVALUE;
-			remander = (remander % QUARTERVALUE);
-
-			double numberOfDimes = remander / DIMEVALUE;
-			remander = (remander % DIMEVALUE);
-
-			double numberOfNickels = remander / NICKELVALUE;
-			remander = (remander % NICKELVALUE);
-
-			System.out.println("toonies:" + numberOfToonies + "; loonies:" + numberOfLoonies + "; quarters:" + (int)numberOfQuarters + "; dimes:" + (int)numberOfDimes + "; nickels:" + (int)numberOfNickels);
 		} catch (Exception e) {
 			System.out.println("INVALID");
 		}
 		
+		
+		if (works) {
+
+			number = NICKELVALUE*(Math.round(number/NICKELVALUE));
+
+			int numberOfToonies = (int)number / (int)TOONIEVALUE;
+			double remainder = (number % TOONIEVALUE);
+
+			int numberOfLoonies = (int)remainder / (int)LOONIEVALUE;
+			remainder = (remainder % LOONIEVALUE);
+
+			double numberOfQuarters = remainder / QUARTERVALUE;
+			remainder = (remainder % QUARTERVALUE);
+
+			double numberOfDimes = remainder / DIMEVALUE;
+			remainder = (remainder % DIMEVALUE);
+
+			double numberOfNickels = remainder / NICKELVALUE;
+			remainder = (remainder % NICKELVALUE);
+
+			System.out.println("toonies:" + numberOfToonies + "; loonies:" + numberOfLoonies + "; quarters:" + (int)numberOfQuarters + "; dimes:" + (int)numberOfDimes + "; nickels:" + (int)numberOfNickels);
+		}
+
 	}
 }
 
